02db9cb3c956a54c8f86dee9f9c15f18dacb3259
Gabriel Pettersson <gabriel.pettersson77@gmail.com>
2024-04-09 16:33:40 +0000

    Istall react.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h2>Hello World!</h2>
+      <Button variant="primary">Secondary</Button>
+      
     </div>
   );
 }

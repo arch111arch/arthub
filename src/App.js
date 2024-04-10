daa22686b140a907d6fc9a23c74f763d43c9baf8
Gabriel Pettersson <gabriel.pettersson77@gmail.com>
2024-04-10 15:40:42 +0000

    Install Route and Switch and add this to App.js and Navbar.js to link header-menu with the render-links.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import './App.css';
-import './components/NavBar';
+import styles from './App.module.css';
 import NavBar from './components/NavBar';
-
+import Container from 'react-bootstrap/Container';
+import {Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     
-    <div className="App">
+    <div className={styles.App}>
       <NavBar/>
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home ase</h1>}></Route>
+            <Route exact path="/signin" render={() => <h1>Sign in</h1>}></Route>
+              <Route exact path="/signup" render={() => <h1>Sign up</h1>}></Route>
+                <Route render={() => <p>Page not found!</p>}></Route>
+        </Switch>
+      </Container>
+
       </div>
   );
 }

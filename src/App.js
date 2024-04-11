9c22c89c40e26f910b4544b5cadcb53f4f4e352f
Gabriel Pettersson <gabriel.pettersson77@gmail.com>
2024-04-11 00:07:44 +0000

    Add formlogic and errorhandling.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import NavBar from './components/NavBar';
 import Container from 'react-bootstrap/Container';
 import {Route, Switch} from 'react-router-dom';
 import './api/axiosDefaults';
-import SignUpForm from './pages/auth/SignInUpForm';
+import SignUpForm from './pages/auth/SignUpForm';
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       <NavBar/>
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() => <h1>Home ase</h1>}></Route>
+          <Route exact path="/" render={() => <h1>Home</h1>}></Route>
             <Route exact path="/signin" render={() => <h1>Sign in</h1>}></Route>
               <Route exact path="/signup" render={() => <SignUpForm/> }></Route>
                 <Route render={() => <p>Page not found!</p>}></Route>
